3a97a06281068bf178c2e702ab5ad1f7e5a51a3b
Frank Luongo <frank@frankluongo.com>
2020-02-05 08:54:43 -0700

    Up to Flash Messages

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,37 @@
-exports.homePage = (req, res) => {
+// exports.myMiddleware = (req, res, next) => {
+//   req.name = 'Wes';
+//   res.cookie('name', 'FrankisCool', { maxAge: 900000 });
+//   next();
+// }
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (_, res) => {
   res.render('index')
+}
+
+exports.addStore = (_, res) => {
+  res.render('editStore', { title: 'Add Store' })
+}
+
+exports.createStore = async (req, res) => {
+  // store
+  //   .save()
+  //   .then(store => {
+  //     res.json(store)
+  //   })
+  //   .catch(err => {
+  //     throw Error(err);
+  //   })
+  // try {
+  //   const store = new Store(req.body);
+  //   await store.save();
+  //   console.log('it worked!')
+  // } catch (err) {
+  //   throw new Error(err);
+  // }
+  const store = new Store(req.body);
+  await store.save();
+  console.log('it worked!')
+  res.redirect('/');
 }
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers')
 
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
+// router.get('/', storeController.myMiddleware, storeController.homePage);
 
 
 
